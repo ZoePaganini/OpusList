eb0a89f339964bfb3835777b4b4ca778425fbc7b
unknown <zoepaganini@paucasesnovescifp.cat>
2021-11-08 15:32:08 +0100

    Control de errores en dialog

---
+++ src/cat/paucasesnovescifp/spdvi/Obra.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.paucasesnovescifp.spdvi;
+
+/**
+ *
+ * @author Alumne
+ */
+class Obra {
+    private String registre;
+    private String titol;
+    private int any;
+    private String format;
+    private String autor;
+    private String imatge;
+
+    public String getRegistre() {
+        return registre;
+    }
+
+    public void setRegistre(String registre) {
+        this.registre = registre;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int any) {
+        this.any = any;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getImatge() {
+        return imatge;
+    }
+
+    public void setImatge(String imatge) {
+        this.imatge = imatge;
+    }
+    
+    public Obra(String registre, String titol, int any, String format, String autor, String imatge)
+    {
+        this.registre = registre;
+        this.titol = titol;
+        this.any = any;
+        this.format = format;
+        this.autor = autor;
+        this.imatge = imatge;
+    }
+    
+    @Override
+    public String toString() {
+        return registre + ": " + titol + ", " + any + " (" + format + "). " + autor; 
+    }
+}
